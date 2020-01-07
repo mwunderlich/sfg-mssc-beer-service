42737d3c205e4ad1bb6297037d9916420319aed4
Martin Wunderlich <martin.wunderlich@censhare.com>
2020-01-07 08:19:31 +0100

    Initial commit

---
+++ src/main/java/guru/springframework/sfgmsscbeerservice/SfgMsscBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgmsscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgMsscBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgMsscBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sfgmsscbeerservice/SfgMsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgmsscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgMsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
