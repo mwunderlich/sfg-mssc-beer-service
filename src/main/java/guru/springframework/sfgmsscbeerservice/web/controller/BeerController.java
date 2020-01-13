c96cb41709523af8f570996a91df09c3ce42a1ca
Martin Wunderlich <martin.wunderlich@censhare.com>
2020-01-13 08:40:06 +0100

    Controller updated

--- src/main/java/guru/springframework/sfgmsscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/sfgmsscbeerservice/web/controller/BeerController.java
@@ -6,12 +6,14 @@ package guru.springframework.sfgmsscbeerservice.web.controller;
 import guru.springframework.sfgmsscbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -24,6 +26,8 @@ import java.util.UUID;
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
+
+    private final BeerService beerService;
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
@@ -44,5 +48,11 @@ public class BeerController {
 
         // TODO Impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void deleteBeer(@PathVariable("beerId") UUID beerId) {
+        beerService.deleteById(beerId);
     }
 }
