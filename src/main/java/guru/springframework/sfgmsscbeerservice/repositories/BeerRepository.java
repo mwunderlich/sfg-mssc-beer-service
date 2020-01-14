587e9721fdc459228932956e966f43ca4d9dc8aa
mwunderlich <martin@wunderlich.com>
2020-01-14 21:17:30 +0100

    Add repo interface

---
+++ src/main/java/guru/springframework/sfgmsscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgmsscbeerservice.repositories;
+
+import guru.springframework.sfgmsscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
