e0ca3ca192eec8e236b310d9ebb8bd85c9ae06e5
mwunderlich <martin@wunderlich.com>
2020-02-10 20:50:22 +0100

    Fix compilation error

--- src/main/java/guru/springframework/sfgmsscbeerservice/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/sfgmsscbeerservice/web/controller/CustomerController.java
@@ -4,11 +4,19 @@
 package guru.springframework.sfgmsscbeerservice.web.controller;
 
 import guru.springframework.sfgmsscbeerservice.services.CustomerService;
-import guru.springframework.sfgmsscbeerservice.web.model.Customer;
+import guru.springframework.sfgmsscbeerservice.web.model.CustomerDto;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
