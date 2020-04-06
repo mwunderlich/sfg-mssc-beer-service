db8c5f4169ba363fc3f63061f95d3dc928d487c4
Martin Wunderlich <martin.wunderlich@censhare.com>
2020-04-06 08:56:37 +0200

    Add caching with ehcache

---
+++ src/main/java/guru/springframework/sfgmsscbeerservice/config/CacheConfig.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) by censhare AG
+ */
+package guru.springframework.sfgmsscbeerservice.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Adds caching config
+ *
+ * @author Martin Wunderlich
+ */
+@EnableCaching
+@Configuration
+public class CacheConfig {
+}
