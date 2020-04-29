5f29cd875c90d5cbd43079c9156c69f033b10818
mwunderlich <martin@wunderlich.com>
2020-04-29 21:41:47 +0200

    Add JSM config

---
+++ src/main/java/guru/springframework/sfgmsscbeerservice/config/JmsConfig.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) by censhare AG
+ */
+package guru.springframework.sfgmsscbeerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * JMS config
+ *
+ * @author Martin Wunderlich
+ */
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter jacksonJmsmessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+}
