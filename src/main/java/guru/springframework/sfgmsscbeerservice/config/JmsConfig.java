0e70ed4a299b46872ee69f82775684757040beb6
mwunderlich <martin@wunderlich.com>
2020-05-23 21:11:52 +0200

    Update BOM; fix compile errors

--- src/main/java/guru/springframework/sfgmsscbeerservice/config/JmsConfig.java
+++ src/main/java/guru/springframework/sfgmsscbeerservice/config/JmsConfig.java
@@ -3,6 +3,7 @@
  */
 package guru.springframework.sfgmsscbeerservice.config;
 
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
